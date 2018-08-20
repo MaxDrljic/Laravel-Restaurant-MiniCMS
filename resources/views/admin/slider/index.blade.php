60d86e1c5f026680d9ead96468d05cc93da0031b
MaxDrljic <max_spree@hotmail.com>
2018-08-20 16:11:37 +0200

    Delete functionality

--- resources/views/admin/slider/index.blade.php
+++ resources/views/admin/slider/index.blade.php
@@ -12,7 +12,7 @@
     <div class="container-fluid">
       <div class="row">
         <div class="col-md-12">
-          <a href="{{ route('slider.create') }}" class="btn btn-info">Add New</a>
+          <a href="{{ route('slider.create') }}" class="btn btn-primary">Add New</a>
           @include('layouts.partial.msg')
           <div class="card">
             <div class="card-header card-header-primary">
@@ -20,7 +20,7 @@
             </div>
             <div class="card-body">
               <div class="table-responsive">
-                <table id="table" class="table table-striped table-bordered" style="width:100%">
+                <table id="table" class="table" style="width:100%">
                   <thead class="text-primary">
                     <th>
                       ID
@@ -53,7 +53,23 @@
                         <td>{{ $slider->image }}</td>
                         <td>{{ $slider->created_at }}</td>
                         <td>{{ $slider->updated_at }}</td>
-                        <td><a href="{{ route('slider.edit', $slider->id) }}" class="btn btn-info btn-sm">Edit</a></td>
+                        <td>
+                          <a href="{{ route('slider.edit', $slider->id) }}" class="btn btn-info btn-sm"><i class="material-icons">mode_edit</i></a>
+
+                          <form id="delete-form-{{ $slider->id }}" action="{{ route('slider.destroy', $slider->id) }}" style="display: none;" method="post">
+                            @csrf
+                            @method('DELETE')
+                          </form>
+                          <button 
+                            type="button" 
+                            class="btn btn-danger btn-sm" 
+                            onclick="if(confirm('Do you want to delete this slider?')){
+                            event.preventDefault();
+                            document.getElementById('delete-form-{{ $slider->id }}').submit();
+                          }else{
+                            event.preventDefault();
+                          }"><i class="material-icons">delete</i></button>
+                        </td>
                       </tr>
                     @endforeach
                   </tbody>
